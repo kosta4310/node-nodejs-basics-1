7e68e01995b2c84c75b5d9e5eb1579d211d35d4a
Pavlov Kostyantyn <realanka77@gmail.com>
2022-12-04 14:41:01 +0200

    feat: add transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,18 @@
+import { Transform } from "node:stream";
+import path from "path";
+import { stdin, stdout } from "node:process";
+import { getDirname } from "../additional/funcDirname.js";
+
+const __dirname = getDirname(import.meta.url);
+
 const transform = async () => {
-    // Write your code here 
+  const transformStream = new Transform({
+    transform(chunk, encoding, callback) {
+      callback(null, String(chunk).trim().split("").reverse().join("") + "\n");
+    },
+  });
+
+  stdin.pipe(transformStream).pipe(stdout);
 };
 
 await transform();
