ab3dc099774f836061a642e7b58fb088b8b72176
Pavlov Kostyantyn <realanka77@gmail.com>
2022-12-04 15:07:38 +0200

    refactor: clean code

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,10 +1,6 @@
-import { Transform, pipeline } from "node:stream";
+import { Transform } from "node:stream";
 import { EOL } from "node:os";
-import path from "path";
 import { stdin, stdout } from "node:process";
-import { getDirname } from "../additional/funcDirname.js";
-
-const __dirname = getDirname(import.meta.url);
 
 const transform = async () => {
   const transformStream = new Transform({
