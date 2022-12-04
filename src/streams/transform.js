b2f1fe0aa761917db73f8612ff29fa3220c778c8
Pavlov Kostyantyn <realanka77@gmail.com>
2022-12-04 15:04:26 +0200

    fix: add eol

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,4 +1,5 @@
-import { Transform } from "node:stream";
+import { Transform, pipeline } from "node:stream";
+import { EOL } from "node:os";
 import path from "path";
 import { stdin, stdout } from "node:process";
 import { getDirname } from "../additional/funcDirname.js";
@@ -7,8 +8,8 @@ const __dirname = getDirname(import.meta.url);
 
 const transform = async () => {
   const transformStream = new Transform({
-    transform(chunk, encoding, callback) {
-      callback(null, String(chunk).trim().split("").reverse().join("") + "\n");
+    transform(chunk, callback) {
+      callback(null, String(chunk).trim().split("").reverse().join("") + EOL);
     },
   });
 
