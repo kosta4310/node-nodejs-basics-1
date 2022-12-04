12663297718f3550a741b5d47a2fed4231de7961
Pavlov Kostyantyn <realanka77@gmail.com>
2022-12-04 14:07:37 +0200

    feat: add write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,14 @@
+import { createWriteStream } from "node:fs";
+import path from "path";
+import { stdin } from "node:process";
+import { getDirname } from "../additional/funcDirname.js";
+
+const __dirname = getDirname(import.meta.url);
+
 const write = async () => {
-    // Write your code here 
+  const filePath = path.resolve(__dirname, "files/fileToWrite.txt");
+  const writeStream = createWriteStream(filePath);
+  stdin.pipe(writeStream);
 };
 
 await write();
