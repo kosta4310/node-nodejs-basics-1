b34d9201580dbc729f8163220d6c198b80355ee7
Pavlov Kostyantyn <realanka77@gmail.com>
2022-12-04 13:56:42 +0200

    feat: add read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,14 @@
+import { createReadStream } from "node:fs";
+import path from "path";
+import { stdout } from "node:process";
+import { getDirname } from "../additional/funcDirname.js";
+
+const __dirname = getDirname(import.meta.url);
+
 const read = async () => {
-    // Write your code here 
+  const filePath = path.resolve(__dirname, "files/fileToRead.txt");
+  const readStream = createReadStream(filePath);
+  readStream.pipe(stdout);
 };
 
 await read();
